1565f8bfb9173474c44fca559ed302417705ebf1
Bruno Martinez <bruno.mtz.vqz@gmail.com>
2024-02-02 09:55:13 -0700

    ejercicio 5

--- parte 1/src/App.jsx
+++ parte 1/src/App.jsx
@@ -3,9 +3,9 @@ const Header = (props) => {
   console.log(props)
   return (
     <div>
-      <p> 
+      <h1> 
        {props.course}
-        </p>
+        </h1>
     </div>
   )
 }
@@ -39,39 +39,40 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1> <Header course={course} /> </h1>
+      <Header course={course.name} /> 
       <div>
-        <Content part={parts[0].name} />  
-        <Total exercises={parts[0].exercises} />
+        <Content part={course.parts[0].name} />  
+        <Total exercises={course.parts[0].exercises} />
       </div>
       <div>
-        <Content part={parts[1].name} />  
-        <Total exercises={parts[1].exercises} />
+        <Content part={course.parts[1].name} />  
+        <Total exercises={course.parts[1].exercises} />
       </div>
       <div>
-        <Content part={parts[2].name} /> 
-        <Total exercises={parts[2].exercises} />
+        <Content part={course.parts[2].name} /> 
+        <Total exercises={course.parts[2].exercises} />
       </div>
-      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
     </div>
   );
 }
